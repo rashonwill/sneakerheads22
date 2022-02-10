c1b719108cc417abb8967ff9ad65c1ad12bc111c
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-02-10 08:57:54 -0600

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,6 +7,7 @@ export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as Product } from "./Product";
 export { default as Cart } from "./Cart";
+export { default as Login } from "./Login";
 
 export { default as Shoes } from "./Shoes";
 export { default as Hats } from "./Hats";
